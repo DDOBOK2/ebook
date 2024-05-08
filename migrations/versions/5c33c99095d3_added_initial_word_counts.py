58687324843584ceb1bcac7d9b1abd00ec4c7215
JUNYOUNG LEE <pbh1527@naver.com>
2024-05-08 22:40:58 +0900

    2final

--- migrations/versions/5c33c99095d3_added_initial_word_counts.py
+++ migrations/versions/5c33c99095d3_added_initial_word_counts.py
@@ -1,10 +1,3 @@
-"""Added initial_word_counts
-
-Revision ID: 5c33c99095d3
-Revises: 97b743a13421
-Create Date: 2024-05-01 21:24:42.783250
-
-"""
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
@@ -15,10 +8,8 @@ down_revision = '97b743a13421'
 branch_labels = None
 depends_on = None
 
-
 def upgrade():
-    # op.add_column('review_session', sa.Column('initial_word_counts', sa.JSON(), nullable=True))
-    pass
+    op.add_column('review_session', sa.Column('initial_word_counts', postgresql.JSON(astext_type=sa.Text()), nullable=True))
 
 def downgrade():
     op.drop_column('review_session', 'initial_word_counts')
